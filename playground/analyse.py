403bc94809c9fd71d47d32d1eed72e27ed32c1f1
JakobHam <72199322+JakobHam@users.noreply.github.com>
2025-10-03 00:09:16 +0200

    first tests of IOLabs

---
+++ playground/analyse.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+df = pd.read_csv("Harmonic_oscilator.csv", skiprows=1,
+                 names=['t', 'ax', 'ay', 'az'])
+
+
+def model(t, delta, A, omega, phi): return np.exp(-delta *
+                                                  t)*(2*A*np.cos(phi + omega*t))
+
+
+popt, pcov = curve_fit(model, df['t'], df['ay'],
+                       sigma=np.sqrt(df['ay']), absolute_sigma=True)
+
+print(popt)
